93d8e5973eab7dbb0e17440a957997bda03cbf33
Dmitriy <dmitiyoleynik@gmail.com>
2020-07-22 19:17:36 +0300

    Add Dragon, Hero and Hit models

---
+++ DataAccessLayer/Models/Dragon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DataAccessLayer.Models
+{
+    public class Dragon
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int HP { get; set; }
+
+        public DateTime CreationDate { get; set; }
+    }
+}
+/*
+Идентификатор (id);+
+Имя (в начале и в конце не должно быть пробелов, первый символ всегда заглавный, латинские символы, пробелы, [4, 20] размер);+
+Количество жизней (целое число от 80 до 100 определяется случайным образом при создании);+
+Время создания дракона в системе (дата и время).
+*/
---
+++ DataAccessLayer/Models/Hit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataAccessLayer.Models
+{
+    public class Hit
+    {
+        public int Power { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int HeroId { get; set; }
+        public int DragonId { get; set; }
+    }
+}
+/*
+ * 
+ Сила удара (какое количество единиц жизни отнимает)*;
+Время удара;+
+Принадлежность к игроку (id);+
+Принадлежность к дракону (id).+
+*Формула расчета силы удара = Сила Оружия героя + (случайное число от 1 до 3)
+*/
