ea0a308eed5c23c122bfecbd3ec81fb18c24622e
Dmitriy <dmitiyoleynik@gmail.com>
2020-07-24 18:40:43 +0300

    Added HitType

---
+++ DragonLibrary'/Models/HitType.cs
@@ -0,0 +1,15 @@
+﻿using GraphQL.Types;
+
+namespace DragonLibrary_.Models
+{
+    public class HitType: ObjectGraphType<Hit>
+    {
+        public HitType()
+        {
+            Field(h => h.HeroId);
+            Field(h => h.DragonId);
+            Field(h => h.Power);
+            Field(h => h.ExecutionTime);
+        }
+    }
+}
